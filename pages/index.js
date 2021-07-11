374f7d3a8d3579ec22f2a4e2c53cbf3d9ace79ec
Vladimir Pestov <pestov.web@yandex.ru>
2021-07-11 20:37:35 +1000

    fixes

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,33 @@
-let popupContainer = document.querySelector('.popup__container');
+let popupContainer = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__info-edit-button');
 let closeButton = document.querySelector('.popup__close-button');
-let saveButton = document.querySelector('.popup__submit-button');
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__form');
 // Находим поля формы в DOM
 let nameInput = document.querySelector('.popup__input-name');
 let jobInput = document.querySelector('.popup__input-prof');
+// Находим имя и проффессию 
+let profileName = document.querySelector(".profile__info-title");
+let profileJob = document.querySelector(".profile__info-subtitle");
 
-
-
+// открываем попап, получаем текст
 function openPopup() {
-  popupContainer.classList.add('popup__opened');
+  popupContainer.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
-
+// закрываем попап
 function closePopup() {
-  popupContainer.classList.remove('popup__opened');
+  popupContainer.classList.remove('popup_opened');
 }
-
+// созраняем новые значения из формы
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  let profileName = document.querySelector(".profile__info-title");
-  let profileJob = document.querySelector(".profile__info-subtitle");
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   closePopup();
 }
-
+// обработать событие
 formElement.addEventListener('submit', formSubmitHandler);
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
